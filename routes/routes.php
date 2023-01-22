59a1da04d1a30309fc47444d5f902397e92c021c
Taylor Hicks <taylor@whatsmyoffer.com>
2023-01-22 17:11:23 -0600

    Changed Namespace and vendor name

--- routes/routes.php
+++ routes/routes.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Taylor\BillingDashboard\Http\Controllers\DashboardController;
+use BitByte\BillingDashboard\Http\Controllers\DashboardController;
 
 Route::middleware('web')->group(function (){
     Route::get('billing', DashboardController::class);
